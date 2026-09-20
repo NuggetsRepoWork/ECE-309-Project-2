7f4a6b7662a8658b873ea5cc8145feb539c3f53d
NuggetsRepoWork <christopherc8206@gmail.com>
2026-09-20 16:19:01 -0400

    Update ECE 309 Project 2.h
    Syntax error in file # include based on file tree repaired

--- ECE 309 Project 2.h
+++ ECE 309 Project 2.h
@@ -1,4 +1,4 @@
-﻿// ECE 309 Project 2.h : Include file for standard system include files,
+// ECE 309 Project 2.h : Include file for standard system include files,
 // or project specific include files.
 
 #pragma once
@@ -12,9 +12,9 @@
 #include <stdexcept>
 #include <utility>
 
-#include "core/message.h"
-#include "core/conversation.h"
-#include "harness/harness.h"
-#include "model/scripted_client.h"
-#include "model/replay_client.h"
+#include "message.h"
+#include "conversation.h"
+#include "harness.h"
+#include "scripted_client.h"
+#include "replay_client.h"
 // TODO: Reference additional headers your program requires here.
