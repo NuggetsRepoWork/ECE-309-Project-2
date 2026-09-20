f5f85a2caafdc3de24f1efd453ac0277ff723a5b
NuggetsRepoWork <christopherc8206@gmail.com>
2026-09-20 13:26:50 -0400

    Add files via upload

---
+++ ECE 309 Project 2.h
@@ -0,0 +1,20 @@
+﻿// ECE 309 Project 2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+
+#include "core/message.h"
+#include "core/conversation.h"
+#include "harness/harness.h"
+#include "model/scripted_client.h"
+#include "model/replay_client.h"
+// TODO: Reference additional headers your program requires here.
